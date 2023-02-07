ac25fb23a7e6d86e15b6d2b0d9cb3eb78172eecd
Kairoon Bodanapu <bodanapukairoon@gmail.com>
2023-02-07 07:41:08 +0530

    my task is completed

--- addfunction.java
+++ addfunction.java
@@ -1,2 +1,9 @@
 this is my add function
+<<<<<<< Updated upstream
 my manager work is completed
+=======
+1st lines of java code completed
+2nd lines of java code completed
+3rd lines of java code completed
+4th lines of java code completed
+5th lines of java code completed
